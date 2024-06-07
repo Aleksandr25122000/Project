f575c36e7ea6bead5e276c1f001f8b4c4805d3a1
mih25 <mikhalko@sfedu.ru>
2024-06-07 20:29:59 +0300

    Final project

---
+++ src/components/articlecomps/connection/connection.js
@@ -0,0 +1,12 @@
+export default function Connection(){
+    return (
+        <div className="connection"><h1>Наши контакты</h1> <br/> <h3>Исполнительный и креативный директор:</h3>
+            Подобедова Алёна Витальевна <br/>
+            podobedova@sfedu.ru
+            <h3>HR-служба:</h3>
+            Михалко Александр Сергеевич <br/>
+            mikhalko@sfedu.ru<br/>
+            +7(961)403 55 21
+        </div>
+    )
+}
